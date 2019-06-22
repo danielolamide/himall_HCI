d62bfca1d8ff2c9af53371b600890282bc0bbcc4
Daniel Olamide <dannyolamide@gmail.com>
2019-06-22 18:28:14 +0300

    DB Connection fix

--- customer/getCategories.php
+++ customer/getCategories.php
@@ -1,4 +1,11 @@
 <?php
     include './DBConnection.php';
     $mysqli = new DBConnection;
+    $res = mysqli_query($mysqli->conn, "SELECT * FROM product_categories");
+
+    if($res-> num_rows> 0){
+        while($row = $res->fetch_assoc()){
+           echo '<li><a href="#">'.$row['category_name'].'</a></li>';
+        }
+    }
 ?>
